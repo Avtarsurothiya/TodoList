3d5f94b924a14179dba43b1b4e2d754dafc989c1
Avtarsurothiya <avtarsurothiya@gmail.com>
2023-08-17 17:00:57 +0530

    Calling all required elements

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//Calling all required elements
+const inputField = document.querySelector(".input-field textarea"),
+  todoLists = document.querySelector(".todoLists"),
+  pendingNum = document.querySelector(".pending-num"),
+  clearButton = document.querySelector(".clear-button");
